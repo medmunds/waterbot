e41866a44258c978239800649fe53bac97e05562
medmunds <medmunds@gmail.com>
2021-04-24 15:40:19 -0700

    Client: upgrade dependencies

--- client/src/views/App.test.js
+++ client/src/views/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../store';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(rootReducer);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div);
 });
